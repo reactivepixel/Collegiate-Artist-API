8c5f5cfade856d3f2b85c38e00cccbce34852bba
Chris Chapman <Chapman@apextion.com>
2017-01-30 21:07:00 -0500

    basic instructions added

--- src/module.js
+++ src/module.js
@@ -1,7 +1,10 @@
-exports.openChest = function() {
-  console.log("The contents of the Chest");
+// Brandon
+exports.addToChest = function() {
+  console.log("Add to the contents of the Chest");
 }
 
+
+// TJ
 exports.chestLocation = function() {
   console.log("X marks the spot");
 }
--- src/server.js
+++ src/server.js
@@ -1,3 +1,14 @@
 var module = require('./module');
 
-module.chestLocation()
+// Simple calling of other module
+module.chestLocation();
+
+
+// TO DO
+// Create a way to store multiple items (Strings) in the chest
+
+
+
+// Pass an X,Y Location into chestLocation,
+// and If it is correct return the contents
+// of the chest.
